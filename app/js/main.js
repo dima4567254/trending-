140aace701f18c7d19a1009c011660e4d8635a2d
dima <dima.xipkov@gmail.com>
2022-06-13 18:40:31 +0300

    first commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,28 @@
 $(function name(params) {
 
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        // direction: 'vertical',
+        loop: true,
+      
+        // If we need pagination
+        // pagination: {
+        //     el: ".swiper-pagination",
+        //     type: "fraction",
+        //   },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        // And if we need scrollbar
+        // scrollbar: {
+        //   el: '.swiper-scrollbar',
+        // },
+      });
+
 
 
     /*	$(".menu a").on("click", function (event) {
